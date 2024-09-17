6f42dc75fd7d13161bc9cf7e92fa616f5e6ad9fb
Andrei Vasiliu <andrei-vasiliu@outlook.com>
2024-09-17 11:30:23 +0100

    feat: add all args and no args constructors for fruit and nutritions

---
+++ app/src/main/java/com/northcoders/fruityvice/model/Fruit.java
@@ -0,0 +1,71 @@
+package com.northcoders.fruityvice.model;
+
+public class Fruit {
+
+    private long id;
+    private String name;
+    private String family;
+    private String order;
+    private String genus;
+    private Nutritions nutritions;
+
+    public Fruit(long id, String name, String family, String order, String genus, Nutritions nutritions) {
+        this.id = id;
+        this.name = name;
+        this.family = family;
+        this.order = order;
+        this.genus = genus;
+        this.nutritions = nutritions;
+    }
+
+    public Fruit() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public String getGenus() {
+        return genus;
+    }
+
+    public void setGenus(String genus) {
+        this.genus = genus;
+    }
+
+    public Nutritions getNutritions() {
+        return nutritions;
+    }
+
+    public void setNutritions(Nutritions nutritions) {
+        this.nutritions = nutritions;
+    }
+}
---
+++ app/src/main/java/com/northcoders/fruityvice/model/Nutritions.java
@@ -0,0 +1,63 @@
+package com.northcoders.fruityvice.model;
+
+public class Nutritions {
+
+    private int calories;
+    private double fat;
+    private double sugar;
+    private double carbohydrates;
+    private double protein;
+
+    //All args constructor
+    public Nutritions(int calories, double fat, double sugar, double carbohydrates, double protein) {
+        this.calories = calories;
+        this.fat = fat;
+        this.sugar = sugar;
+        this.carbohydrates = carbohydrates;
+        this.protein = protein;
+    }
+
+    //No args constructor
+    public Nutritions() {
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getSugar() {
+        return sugar;
+    }
+
+    public void setSugar(double sugar) {
+        this.sugar = sugar;
+    }
+
+    public double getCarbohydrates() {
+        return carbohydrates;
+    }
+
+    public void setCarbohydrates(double carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+}
